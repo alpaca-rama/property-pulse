b88dbae8992b550a94ff84f399d65f0e72b615e8
Luca Capriati <luca@weve-got-it.com>
2024-02-26 22:07:35 +0200

    Created Files
    Created layout and styles

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+    title: 'Property Pulse | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties'
+}
+export default function MainLayout({ children }) {
+    return (
+        <html lang={'en'}>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
