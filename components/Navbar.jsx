ccccfc09a76d74297880c07db7706b820839d5cc
Luca Capriati <luca@weve-got-it.com>
2024-02-27 10:55:54 +0200

    Update Navbar.jsx
    Added active links css for the desktop and mobile menu.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 import { FaGoogle } from 'react-icons/fa'
 
@@ -12,6 +13,7 @@ import {useState} from "react";
 export default function Navbar() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
+    const pathname = usePathname()
 
     return (
         <nav className="bg-blue-700 border-b border-blue-500">
@@ -68,19 +70,19 @@ export default function Navbar() {
                             <div className="flex space-x-2">
                                 <Link
                                     href="/"
-                                    className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={`${pathname === '/' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                                 >
                                     Home
                                 </Link>
                                 <Link
                                     href="/properties"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                                 >
                                     Properties
                                 </Link>
                                 <Link
                                     href="/properties/add"
-                                    className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                    className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                                 >
                                     Add Property
                                 </Link>
@@ -201,17 +203,17 @@ export default function Navbar() {
                     <div className="space-y-1 px-2 pb-3 pt-2">
                         <Link
                             href="/"
-                            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                            className={`${pathname === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
                         >Home</Link
                         >
                         <Link
                             href="/properties"
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                            className={`${pathname === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
                         >Properties</Link
                         >
                         <Link
                             href="/properties/add"
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                            className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
                         >Add Property</Link
                         >
                         <button
