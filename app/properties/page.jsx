52c8a46ba96dc9e5f3e2b0fba5cbdf7a7c36b763
Luca Capriati <lucapriati246@gmail.com>
2024-03-05 21:08:21 +0200

    Added Feature - Pagination
    - Refactored property components to be a client component to ensure the whole page it is placed on is not client rendered.
    
    - Implemented the pagination.
    
    - Added the pagination component.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,17 +1,11 @@
-import PropertyCard from "@/components/PropertyCard";
 import {fetchProperties} from "@/utils/request";
 import PropertySearchForm from "@/components/PropertySearchForm";
 import Spinner from "@/components/Spinner";
 import Link from "next/link";
 import {FaArrowAltCircleLeft} from "react-icons/fa";
+import Properties from "@/components/Properties";
 
 export default async function PropertiesPage() {
-    const properties = await fetchProperties()
-
-    // Sort properties by date
-    properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-
-
     return (
         <>
             <section className={'bg-blue-700 py-4'}>
@@ -20,19 +14,7 @@ export default async function PropertiesPage() {
                 </div>
             </section>
 
-            <section className="px-4 py-6">
-                <div className="container-xl lg:container m-auto px-4 py-6">
-                    { properties.length === 0 ? (
-                        <p>No properties found</p>
-                    ) : (
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                            {properties.map(property => (
-                                <PropertyCard key={property._id} property={property} />
-                            ))}
-                        </div>
-                    )}
-                </div>
-            </section>
+            <Properties />
         </>
     )
 }
